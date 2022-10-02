025607f4df6e63beb5a6324f12686bd85b0a6412
mayankgupta804 <mayank.gupta804@gmail.com>
2022-10-02 11:57:04 +0530

    Initial commit: Render movies data

---
+++ src/components/movies.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {};
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>Showing {this.state.movies.length} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td><button type="button" className="btn btn-danger btn-sm">Delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
